e4f5dd49d6732ca3433c1cca18b143c50ec714ec
nowshadjunaed <nowshadjunaed@gmail.com>
2020-10-19 00:52:35 +0600

    some changes

--- lib/screens/confirm_booking.dart
+++ lib/screens/confirm_booking.dart
@@ -338,6 +338,55 @@ class _ConfirmBookingState extends State<ConfirmBooking> {
                           ),
                         );
                       });
+                  showGeneralDialog(
+
+                      context: context,
+                      barrierDismissible: true,
+                      barrierLabel: MaterialLocalizations.of(context)
+                          .modalBarrierDismissLabel,
+                      barrierColor: Colors.black45,
+                      transitionDuration: const Duration(milliseconds: 200),
+                      pageBuilder: (BuildContext buildContext,
+                          Animation animation,
+                          Animation secondaryAnimation) {
+                        return Center(
+
+
+                          child: Container(
+
+                            width: MediaQuery.of(context).size.width - 0,
+                            height: MediaQuery.of(context).size.height-150,
+                            padding: EdgeInsets.all(20),
+                            color: Colors.white,
+                            child : Card(
+                              elevation: 0,
+                              child: Container(
+                                //padding: EdgeInsets.only(top: 20),
+                                height: 400,
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  children: <Widget>[
+                                    SelectableText("Please make sure that you have copied the Booking Id : " + booking_id + "\n",style: TextStyle(color: Colors.black, fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),),
+                                    RaisedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => SearchScreenBody()),);
+                                      },
+                                      child: Text(
+                                        "OK",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      color: const Color(0xFF1BC0C5),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),//card
+                          ),
+                        );
+                      });
                 },
                 padding: EdgeInsets.all(15.0),
                 shape: RoundedRectangleBorder(
